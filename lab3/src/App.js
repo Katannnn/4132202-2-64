01f89be993c13617752f14dc26cf25fa512f04e4
Katannnn <630112418001@bru.ac.th>
2021-12-13 12:02:06 +0700

    LAB3

--- lab3/src/App.js
+++ lab3/src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://facebook.com/katan.fifa"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          กตัณณ์
         </a>
       </header>
     </div>
